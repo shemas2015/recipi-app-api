846b87104fb4158b924bae7bc064ee7d8e439df8
Sebastián Daza <juan.d@montechelo.com>
2023-09-12 20:42:00 -0500

    drf_spectacular for documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +85,7 @@ DATABASES = {
         'USER': os.environ.get("DB_USER") ,
         'PASSWORD': os.environ.get("DB_PASS") ,
     }
-    
+
 }
 
 
@@ -129,3 +131,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK ={
+    "DEFAULT_SCHEMA_CLASS": 'drf_spectacular.openapi.AutoSchema',
+}
